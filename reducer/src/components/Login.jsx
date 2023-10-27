55fe38a60702091d7557e0152821fac39ea9d784
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-27 23:50:01 +0530

    Update Login.jsx

--- reducer/src/components/Login.jsx
+++ reducer/src/components/Login.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+        const [user,setUser] = useState({email:"",password:""})
+        const handleSubmit = (e) => {
+                e.preventDefault()
+                localStorage.setItem("user",JSON.stringify(user))
+                // localStorage.clear()
+        }
+        const handleInputChange = (e) => {
+                setUser({...user,[e.target.name]:e.target.value})
+        }
   return (
     <div className='container'>
         <form className='container-form'>
                 <h2 className='form-header'>Login Form</h2>
                 <label>Email</label>
-                <input className='form-input' type='email' placeholder='Enter Email'/>
+                <input className='form-input' name='email' type='email' placeholder='Enter Email' onChange={handleInputChange}/>
                 <label>Password</label>
-                <input className='form-input' type='password' placeholder='Enter Password'/>
-                <button className='btn'>Login</button>
+                <input className='form-input' name='password' type='password' placeholder='Enter Password' onChange={handleInputChange}/>
+                <button className='btn' onClick={handleSubmit}>Login</button>
         </form>
     </div>
   )
