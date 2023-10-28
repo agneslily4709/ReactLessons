582d69a1b3f8300d451a6c7f18022e321bb49f8b
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-28 13:46:47 +0530

    Update Navbar.jsx

--- reducer/src/components/Navbar.jsx
+++ reducer/src/components/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link} from 'react-router-dom'
+import userReducer from '../reducer/authReducer'
+import { UserContext } from '../context/userContext'
 const Navbar = () => {
+        const {authState,dispatch} = useContext(UserContext)
+        console.log(authState)
   return (
     <div className='navbar'>
         <h1>Reducer Lesson</h1>
         <div className='navbar-items'>
+        {authState.isLoggedIn?
+        <>
+                <Link to="/profile"><button className='navbar-btns'>Profile</button></Link>
+                <Link to="/logout"><button className='navbar-btns'>Logout</button></Link>
+        </>
+        :
+        <>
                 <Link to="/login"><button className='navbar-btns'>Login</button></Link>
                 <Link to="/signup"><button className='navbar-btns'>Sign up</button></Link>
+        </>
+        }
         </div>
     </div>
   )
