3f40817a7d633d03339bf2f7b8653db06d48bf25
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-28 13:46:13 +0530

    Update App.js

--- reducer/src/App.js
+++ reducer/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
+import { Logout } from './components/Logout';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
                                 <Route path="/" element={<Home/>}/>
                                 <Route path="/login" element={<Login/>}/>
                                 <Route path="/signup" element={<Signup/>}/>
+                                <Route path="/logout" element={<Logout/>}/>
+                                <Route path="/profile" element={<Profile/>}/>
                         </Routes>
                 </BrowserRouter>
         </>
