1913dd098d1d80381d28016a5b4b62556f3ec0f9
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-29 23:48:30 +0530

    Update Logout.jsx

--- reducer/src/components/Logout.jsx
+++ reducer/src/components/Logout.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const Logout = () => {
+        useEffect(()=>{
+                if(localStorage.getItem("user"))localStorage.clear()
+                
+        },[])
   return (
     <div>Logout</div>
   )
