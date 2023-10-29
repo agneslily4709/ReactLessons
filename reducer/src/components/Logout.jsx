07ca68282f4ec4053b9e7f85eb1d673b7f88f70e
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-29 23:48:50 +0530

    Update Logout.jsx

--- reducer/src/components/Logout.jsx
+++ reducer/src/components/Logout.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Logout = () => {
+        const navigate = useNavigate()
         useEffect(()=>{
                 if(localStorage.getItem("user"))localStorage.clear()
-                
+                navigate("/login")
         },[])
   return (
     <div>Logout</div>
