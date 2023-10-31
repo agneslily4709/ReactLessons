0d89e2b86459f5241d73fb6a762a25da0c519a5e
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-31 22:57:12 +0530

    Create App.js

---
+++ pagination/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+        <>
+        <h1>Pagination Task</h1>
+        <Home/>
+        </>
+  );
+}
+
+export default App;
